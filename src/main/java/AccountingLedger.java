98ddc512f18d34b61a69097991e257b55f9fcfdb
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-29 14:46:27 -0500

    Added the scanner

--- src/main/java/AccountingLedger.java
+++ src/main/java/AccountingLedger.java
@@ -1,3 +1,5 @@
+
+
 /***
  * Mikayla Brabham
  * Capstone One (Workbook 1a - 3b)
@@ -8,8 +10,12 @@
  * Citations:
  *
  */
+//Import Scanner
+import java.util.Scanner;
 
 public class AccountingLedger {
+    //Create a scanner to gather user data when called
+    Scanner myScanner = new Scanner(System.in);
 
     //add main method to project class
     public static void main(String[] args) {
