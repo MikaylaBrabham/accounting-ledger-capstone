c8e0a0e24b6739a35f46d447b95a38d715459444
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-29 14:41:01 -0500

    Added meaningful comment for my understanding later down the line

--- src/main/java/AccountingLedger.java
+++ src/main/java/AccountingLedger.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+/***
+ * Mikayla Brabham
+ * Capstone One (Workbook 1a - 3b)
+ * Accounting Ledger
+ * Instructor: Eric Schwartz
+ * YearUpUnited 5/1/2026
+ *
+ * Citations:
+ *
+ */
 
 public class AccountingLedger {
+
+    //add main method to project class
+    public static void main(String[] args) {
+
+    }
 }
