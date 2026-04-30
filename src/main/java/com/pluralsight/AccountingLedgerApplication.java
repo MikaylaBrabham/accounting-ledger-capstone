f884c578c2d8ca324b553d96140787a77bee56ad
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-30 12:56:47 -0500

    Added the scanner

---
+++ src/main/java/com/pluralsight/AccountingLedgerApplication.java
@@ -0,0 +1,32 @@
+/***
+ * Mikayla Brabham
+ * Capstone One (Workbook 1a - 3b)
+ * Accounting Ledger
+ * Instructor: Eric Schwartz
+ * YearUpUnited 5/1/2026
+ *
+ * Citations:
+ *
+ */
+//Import Scanner
+package com.pluralsight;
+// import buffreader, filereader, scanner
+import java.io.BufferedReader;
+import java.io.*;
+import java.util.Scanner;
+import java.io.FileWriter;
+
+public class AccountingLedgerApplication {
+    //Add main method
+    public static void main(String[] args) {
+        //add main method to project class
+        //Create a scanner to gather user data when called
+        Scanner myScanner = new Scanner(System.in);
+
+        //add a header introduction to the file
+        System.out.println("***--- Mikayla's Accounting Ledger ---***");
+
+        myScanner.close();
+
+    }
+}
